121f14115859c0404b77ade43ce780fe90d1c633
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2023-06-23 17:20:50 -0500

    Show progress bar

--- run-medperf/bg-setup.sh
+++ run-medperf/bg-setup.sh
@@ -1,7 +1,5 @@
-sleep 15
 git clone https://github.com/mlcommons/medperf
 cd medperf/server
-ls
 pip install -r requirements.txt
 pip install -r test-requirements.txt
 sh setup-dev-server.sh
--- run-medperf/setup.sh
+++ run-medperf/setup.sh
@@ -1,3 +1,23 @@
-git clone https://github.com/mlcommons/medperf
+#!/bin/bash
+echo -ne '                        (0%)  Getting medperf   \r'
+git clone https://github.com/mlcommons/medperf > /dev/null 2>&1
 cd medperf
-pip install -e cli
+echo -ne '####                    (17%) Installing medperf\r'
+pip install -e cli > /dev/null 2>&1
+echo -ne '########                (33%) Waiting on server \r'
+
+work_done=false
+until $work_done; do
+   wget https://localhost:8000 > /dev/null 2>&1
+   if [[ $? -eq 5 ]]
+   then
+      work_done=true
+   fi
+   i=$((i+1))
+done
+
+echo -ne '############            (50%) Seeding server    \r'
+cd server
+python seed.py --cert=cert.crt > /dev/null 2>&1
+
+echo -ne '################        (67%) Done!             \r'
