5e605eb0f76916f27c4bf1e9fe910973fe628766
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2023-06-26 10:42:17 -0500

    Rename script

--- run-medperf/fg.sh
+++ run-medperf/fg.sh
@@ -1,4 +1,4 @@
 echo "Retrieving the setup script"
-wget https://shorturl.at/cALNV  > /dev/null 2>&1
+wget -O setup-medperf.sh https://shorturl.at/cALNV  > /dev/null 2>&1
 chmod a+x setup-medperf.sh
 bash setup-medperf.sh
