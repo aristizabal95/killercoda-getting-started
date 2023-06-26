f42ab9c9d084663eec71608ebf97e1430c7c768b
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2023-06-26 10:40:46 -0500

    use a shorter link

--- run-medperf/fg.sh
+++ run-medperf/fg.sh
@@ -1,4 +1,4 @@
 echo "Retrieving the setup script"
-wget https://gist.githubusercontent.com/aristizabal95/227b2bf9ff4ddb87b4e81b89b2c4f4b7/raw/8d3f7b29fdb18722e4e90ece36aa98b45aa396cf/setup-medperf.sh
+wget https://shorturl.at/cALNV  > /dev/null 2>&1
 chmod a+x setup-medperf.sh
 bash setup-medperf.sh
