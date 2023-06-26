f77a5441a6a30657775564ba72503e43e0e21839
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2023-06-26 15:49:44 -0500

    Update setup link. Remove messy print

--- run-medperf/fg.sh
+++ run-medperf/fg.sh
@@ -1,47 +1,6 @@
 echo "Retrieving the setup script"
-wget -O setup-medperf.sh https://shorturl.at/cdgkV  > /dev/null 2>&1
+wget -O setup-medperf.sh https://shorturl.at/cfOS2  > /dev/null 2>&1
 chmod a+x setup-medperf.sh
 bash setup-medperf.sh
 cd medperf
 medperf profile activate test
-cat << EOF
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  __  __          _                  __   _____      _             _       _ 
- |  \/  | ___  __| |_ __   ___ _ __ / _| |_   _|   _| |_ ___  _ __(_) __ _| |
- | |\/| |/ _ \/ _` | '_ \ / _ \ '__| |_    | || | | | __/ _ \| '__| |/ _` | |
- | |  | |  __/ (_| | |_) |  __/ |  |  _|   | || |_| | || (_) | |  | | (_| | |
- |_|  |_|\___|\__,_| .__/ \___|_|  |_|     |_| \__,_|\__\___/|_|  |_|\__,_|_|
-                   |_|                                                       
-
-EOF
