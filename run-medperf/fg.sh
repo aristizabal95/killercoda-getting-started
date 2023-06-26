473f14838d16da91d4380c52da28469127246c59
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2023-06-26 10:58:23 -0500

    Fix Issues, improve UX

--- run-medperf/fg.sh
+++ run-medperf/fg.sh
@@ -1,5 +1,5 @@
 echo "Retrieving the setup script"
-wget -O setup-medperf.sh https://shorturl.at/kwGJ7  > /dev/null 2>&1
+wget -O setup-medperf.sh https://shorturl.at/fjuW2  > /dev/null 2>&1
 chmod a+x setup-medperf.sh
 bash setup-medperf.sh
 cd medperf
